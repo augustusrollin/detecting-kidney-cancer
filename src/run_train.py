e218610bedb0169953e145263de8d088ba570496
Augustus Rollin <pikachu@reactsoftwareinc.com>
2024-10-04 14:09:47 -0400

    run

--- src/run_train.py
+++
@@ -1,7 +0,0 @@
-from TrainingManager import TrainingManager
-
-if __name__ == "__main__":
-    csv_path = 'kidneyData.csv'
-    img_dir = 'kidney_ct_data'
-    training_manager = TrainingManager(csv_path, img_dir, epochs=50)
-    training_manager.train_model(csv_path, img_dir)
