728109e35adea87b27468aad5c4c58cf18d33136
Augustus Rollin <pikachu@reactsoftwareinc.com>
2024-10-11 10:11:02 -0400

    sample data / tests

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -5,13 +5,7 @@ from EvaluationManager import EvaluationManager
 
 
 # @pytest.mark.skip(reason="takes a while to run")
-def test_evaluate():
-    data_folder = "kidney_ct_data"
-
+def test_evaluate(data_folder, csv_path):
     os.makedirs(data_folder, exist_ok=True)
-
-    csv_path = 'kidneyData.csv'
-    img_dir = 'kidney_ct_data'  
-
-    evaluation_manager = EvaluationManager(csv_path, img_dir)
-    evaluation_manager.evaluate_model(csv_path, img_dir)
+    evaluation_manager = EvaluationManager(csv_path, data_folder)
+    evaluation_manager.evaluate_model(csv_path, data_folder)
--- tests/test_training.py
+++ tests/test_training.py
@@ -5,13 +5,7 @@ from TrainingManager import TrainingManager
 
 
 # @pytest.mark.skip(reason="takes a while to run")
-def test_train():
-    data_folder = "kidney_ct_data"
-
+def test_train(data_folder, csv_path):
     os.makedirs(data_folder, exist_ok=True)
-
-    csv_path = 'kidneyData.csv'
-    img_dir = 'kidney_ct_data'  
-
-    training_manager = TrainingManager(csv_path, img_dir)
-    training_manager.train_model(csv_path, img_dir)
+    training_manager = TrainingManager(csv_path, data_folder)
+    training_manager.train_model(csv_path, data_folder)
